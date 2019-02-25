b48e27d344ad3de03bcd699057e9574b108c1c6a
Marco Bernasocchi <marco@opengis.ch>
2019-02-25 18:43:10 +0100

    initial code commit

---
+++ __init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+    QGIS Server Plugin Filters: Add a new request to invalidate QGIS server
+    cache of a project
+    ---------------------
+    Date                 : February 2019
+    Copyright            : (C) 2019 by Marco Bernasocchi - OPENGIS.ch
+    Email                : marco at opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+ This script initializes the plugin, making it known to QGIS and QGIS Server.
+"""
+try:
+    from qgis.server import *
+except:
+    pass
+
+
+def serverClassFactory(serverIface):  # pylint: disable=invalid-name
+    """Load InvalidateCacheServer class from file invalidate_cache_server.
+
+    :param iface: A QGIS Server interface instance.
+    :type iface: QgsServerInterface
+    """
+    #
+    from .invalidate_cache_server import InvalidateCacheServer
+    return InvalidateCacheServer(serverIface)
---
+++ invalidate_cache_server.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+    QGIS Server Plugin Filters: Add a new request to invalidate QGIS server
+    cache of a project
+    ---------------------
+    Date                 : February 2019
+    Copyright            : (C) 2019 by Marco Bernasocchi - OPENGIS.ch
+    Email                : marco at opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+__author__ = 'Marco Bernasocchi'
+__date__ = 'February 2019'
+__copyright__ = '(C) 2019, Marco Bernasocchi - OPENGIS.ch'
+
+from qgis.core import QgsMessageLog, QgsLogger
+
+
+class InvalidateCacheServer:
+    """Plugin for QGIS server
+    this plugin loads invalidatecache filter"""
+
+    def __init__(self, serverIface):
+        # Save reference to the QGIS server interface
+        self.serverIface = serverIface
+        QgsMessageLog.logMessage("SUCCESS - invalidatecache init",
+                                 'plugin',
+                                 QgsMessageLog.INFO)
+
+        from .filters.invalidate_cache_filter import InvalidateCacheFilter
+        try:
+            serverIface.registerFilter(InvalidateCacheFilter(serverIface), 50)
+        except Exception as e:
+            QgsLogger.debug("invalidatecache - Error loading filter: %s" % e )

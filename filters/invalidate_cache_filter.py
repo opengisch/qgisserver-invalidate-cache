c0e91844b21691f87f438a3fc9254e5bb30a401f
Marco Bernasocchi <marco@opengis.ch>
2019-02-25 19:15:58 +0100

    add filter - this is the most basic server plugin possible

---
+++ filters/invalidate_cache_filter.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+    QGIS Server Plugin Filters: Add a new request to invalidate QGIS server
+    cache of a project
+    ---------------------
+    Date                 : February 2019
+    Copyright            : (C) 2017 by Michaël Douchin - 3Liz
+                           (C) 2019 by Marco Bernasocchi - OPENGIS.ch
+    Email                : marco at opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import json
+import os
+
+from configparser import ConfigParser
+from qgis.core import QgsMessageLog
+from qgis.server import *
+
+
+class InvalidateCacheFilter(QgsServerFilter):
+    metadata = {}
+
+    def __init__(self, serverIface):
+        QgsMessageLog.logMessage("InvalidateCacheFilter.init")
+        super(InvalidateCacheFilter, self).__init__(serverIface)
+        self.serverIface = serverIface
+        self.request = None
+        self.project = None
+        self.project_path = None
+        self.debug_mode = True
+
+        self.getMetadata()
+
+        # syslog.syslog(syslog.LOG_ERR, "INVALIDATECACHE - INITIALIZE")
+
+    def getMetadata(self):
+        '''
+        Get plugin metadata
+        '''
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        mfile = os.path.join(dir_path, '../metadata.txt')
+        if os.path.isfile(mfile):
+            config = ConfigParser()
+            config.read(mfile)
+            self.metadata = {
+                'name': config.get('general', 'name'),
+                'version': config.get('general', 'version')
+                }
+
+    def setJsonResponse(self, status, body):
+        '''
+        Set response with given parameters
+        '''
+        self.request.clearHeaders()
+        self.request.setInfoFormat('text/json')
+        self.request.setHeader('Content-type', 'text/json')
+        self.request.setHeader('Status', status)
+        self.request.clearBody()
+        self.request.appendBody(json.dumps(body))
+
+    def responseComplete(self):
+        '''
+        Send new response
+        '''
+
+        self.request = self.serverIface.requestHandler()
+        params = self.request.parameterMap()
+
+        # Check if interesting request. If not, just send the response
+        if 'REQUEST' not in params or params['REQUEST'].lower() not in [
+            'invalidatecache', 'getcapabilitiesinvalidatecache']:
+            return
+
+        # Get capabilities
+        if params['REQUEST'].lower() == 'getcapabilitiesinvalidatecache':
+            body = {
+                'status': 'success',
+                'metadata': self.metadata
+                }
+            self.setJsonResponse('200', body)
+            return
+
+        # Check if needed params are set
+        if 'MAP' not in params:
+            body = {
+                'status': 'fail',
+                'message': 'Missing parameters: MAP is required '
+                }
+            self.setJsonResponse('200', body)
+            return
+
+        self.project_path = params['MAP']
+
+        QgsConfigCache.instance().removeEntry(self.project_path)
+
+        body = {
+            'MAP': self.project_path,
+            'status': 'success',
+            'message': 'cache cleared'
+            }
+        self.setJsonResponse('200', body)
+
+        return
